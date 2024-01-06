826a4f4cd4591715cc5386a9d2679f699c9ba566
András Nagy <andrasnagy097@gmail.com>
2024-01-06 15:41:42 +0100

    make ping path

--- app/drf_project/urls.py
+++ app/drf_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_project.views import ping
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("ping/", ping, name="ping"),
     path("", include("movies.urls")),
 ]
